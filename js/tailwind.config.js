c4b09201d77bfc31766b3cc36b8bb0ee12f954d8
daovietanh99 <daovietanh190499dev@gmail.com>
2024-07-28 09:59:45 +0700

    init

---
+++ js/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      extend: {      
+        colors: {
+          lavalemenu: '#001114',
+          lavale: '#003a47',
+          lavalelight: "#008391",
+          lavalesemi: "#073b47",
+          active: '#feefc0',
+          advice: '#f5f2e3',
+          footer: "#061d23",
+        },
+        fontFamily: {
+          display: ["Playfair Display", 'serif'],
+          monsterrat: ["Montserrat", "sans-serif"],
+        },
+        boxShadow: {
+          'homepage': '0 200px 0 200px #003a47',
+        }
+      }
+    },
+  }
