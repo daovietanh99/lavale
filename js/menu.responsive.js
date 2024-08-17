984e5f72027c9f99a745d8b6c240ede167e8271a
daovietanh99 <daovietanh190499dev@gmail.com>
2024-08-17 13:04:13 +0700

    add responsive menu

---
+++ js/menu.responsive.js
@@ -0,0 +1,11 @@
+class MenuResponsive {
+    init() {
+        this.full_menu = document.querySelectorAll('[data-menu="full-menu"]');
+        this.simple_menu_show = document.querySelectorAll('[data-menu="simple-menu-show"]');
+        this.simple_menu_popup = document.querySelectorAll('[data-menu="simple-menu-popup"]');
+        this.toggle_open = document.querySelectorAll('[data-menu="toggle-open"]');
+        this.toggle_close = document.querySelectorAll('[data-menu="toggle-close"]');
+
+        
+    }
+}
